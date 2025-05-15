adaa2e06073dd87c5961c1654e685d17c79b9f8d
lioooonn <davidlyons2007@gmail.com>
2025-05-15 11:45:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -177,6 +177,15 @@ function draw() {
 
     drawMatrix(arena, {x: 0, y: 0}, context);
 
+    // TEMP
+
+    context.globalAlpha = 0.3;
+    context.fillStyle = '#0f0';
+    context.fillRect(0, 18, 10, 1);
+    context.globalAlpha = 1.0;
+    
+    // TEMP
+
     // Draw ghost piece
     const ghost = getGhostPosition(player, arena);
     context.globalAlpha = 0.3;  // lower opacity
